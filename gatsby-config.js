e343adabb7c5acea9a849fbbc9ebc550af77f856
Ryan Timms <ryantimms92@gmail.com>
2019-08-10 08:50:53 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        path: `${__dirname}/content/reviews`,
+        name: `reviews`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         path: `${__dirname}/content/assets`,
         name: `assets`,
       },
